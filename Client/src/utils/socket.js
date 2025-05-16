24577084e4685be7721a3ee72b58e4592160de37
Anshika <anshika1366@gmail.com>
2025-05-16 20:19:14 +0530

    update io

--- Client/src/utils/socket.js
+++ Client/src/utils/socket.js
@@ -4,7 +4,23 @@ import { SOCKET_URL } from '../config';
 // Create a singleton socket instance
 const socket = io(SOCKET_URL, {
   withCredentials: true,
-  autoConnect: true
+  autoConnect: true,
+  transports: ['websocket', 'polling'],
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000
+});
+
+// Add connection event listeners
+socket.on('connect', () => {
+  console.log('Connected to socket server');
+});
+
+socket.on('connect_error', (error) => {
+  console.error('Socket connection error:', error);
+});
+
+socket.on('disconnect', (reason) => {
+  console.log('Disconnected from socket server:', reason);
 });
 
 export default socket; 
